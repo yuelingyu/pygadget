8b08ad5685467fd3f592a93930bf0d62a5c85bfe
岳凌宇/Lingyu Yue <yuelymechanics@gmail.com>
2021-11-29 10:44:18 -0500

    Create color_schemes.py

---
+++ mplstyle/color_schemes.py
@@ -0,0 +1,6 @@
+# https://personal.sron.nl/~pault/#sec:qualitative
+
+# Color Bright
+colors = ['#4477AA', '#EE6677',  '#228833', '#CCBB44', '#66CCEE', '#AA3377', '#BBBBBB']
+# Color Variant
+colors = ['#0077BB', '#CC3311', '#009988', '#EE7733', '#EE3377', '#33BBEE', '#BBBBBB']
